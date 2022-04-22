2c3a34dc8ffdf02448b2581d652149de6171b08b
Jakub Cabal <cabal@cesnet.cz>
2022-04-22 15:20:47 +0200

    NET_MOD_CORE: [MAINTENANCE] Clean up Modules.tcl

--- intel/src/comp/network_mod/comp/network_mod_core/Modules.tcl
+++ intel/src/comp/network_mod/comp/network_mod_core/Modules.tcl
@@ -4,8 +4,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Set paths
-
 # Paths to components
 set RX_ADAPTER_BASE     "$OFM_PATH/comp/nic/mac_lite/rx_mac_lite/comp/adapters/"
 set TX_ADAPTER_BASE     "$OFM_PATH/comp/nic/mac_lite/tx_mac_lite/comp/adapters/"
@@ -16,65 +14,59 @@ set DK_AGI_IP_BASE      "$CARDS_BASE/dk-dev-agi027res/src/ip"
 set AGI_FH400G_IP_BASE  "$CARDS_BASE/agi-fh400g/src/ip"
 
 # Packages
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/math_pack.vhd"
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/type_pack.vhd"
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/eth_hdr_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/math_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/type_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/eth_hdr_pack.vhd"
 
-set MOD "$MOD $ENTITY_BASE/network_mod_core_ent.vhd"
+set COMPONENTS [concat $COMPONENTS [list \
+    [ list "MI_INDIRECT_ACCESS"    "$MI_TOOLS_BASE/indirect_access"  "FULL" ] \
+]]
 
-if { $ARCHGRP == "400G1" } {
+lappend MOD "$ENTITY_BASE/network_mod_core_ent.vhd"
 
-    set COMPONENTS [concat $COMPONENTS [list \
-        [ list "TX_FTILE_ADAPTER"    "$TX_ADAPTER_BASE/mac_seg"  "FULL" ] \
-        [ list "RX_FTILE_ADAPTER"    "$RX_ADAPTER_BASE/mac_seg"  "FULL" ] \
-    ]]
-
-    # Source files for implemented component
-    # now in fpga top-level
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_1x400g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_1x400g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_2x200g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_2x200g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_4x100g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_4x100g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_8x50g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_8x50g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_2x40g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_2x40g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_8x25g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_8x25g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_pll_8x10g.ip"
-    set MOD "$MOD $AGI_FH400G_IP_BASE/ftile_eth_8x10g.ip"
-
-    set MOD "$MOD $ENTITY_BASE/network_mod_core_ftile.vhd"
-    #set MOD "$MOD $ENTITY_BASE/DevTree.tcl"
-
-} elseif { $ARCHGRP == "DK-DEV-AGI027RES" } {
+if { $ARCHGRP == "400G1" || $ARCHGRP == "DK-DEV-AGI027RES"} {
 
     set COMPONENTS [concat $COMPONENTS [list \
         [ list "TX_FTILE_ADAPTER"    "$TX_ADAPTER_BASE/mac_seg"  "FULL" ] \
         [ list "RX_FTILE_ADAPTER"    "$RX_ADAPTER_BASE/mac_seg"  "FULL" ] \
     ]]
 
-    # Source files for implemented component
-    # now in fpga top-level
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_1x400g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_1x400g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_2x200g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_2x200g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_4x100g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_4x100g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_8x50g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_8x50g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_2x40g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_2x40g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_8x25g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_8x25g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_pll_8x10g.ip"
-    set MOD "$MOD $DK_AGI_IP_BASE/ftile_eth_8x10g.ip"
+    # IP are now in card (400G1) top-level Modules.tcl
+    # Uncomment for network module synthesis only!
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_1x400g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_1x400g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_2x200g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_2x200g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_4x100g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_4x100g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_8x50g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_8x50g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_2x40g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_2x40g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_8x25g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_8x25g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_pll_8x10g.ip"
+    #lappend MOD "$AGI_FH400G_IP_BASE/ftile_eth_8x10g.ip"
 
-    set MOD "$MOD $ENTITY_BASE/network_mod_core_ftile.vhd"
-    #set MOD "$MOD $ENTITY_BASE/DevTree.tcl"
+    # IP are now in card (DK-DEV-AGI027RES) top-level Modules.tcl
+    # Uncomment for network module synthesis only!
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_1x400g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_1x400g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_2x200g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_2x200g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_4x100g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_4x100g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_8x50g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_8x50g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_2x40g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_2x40g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_8x25g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_8x25g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_pll_8x10g.ip"
+    #lappend MOD "$$DK_AGI_IP_BASE/ftile_eth_8x10g.ip"
+
+    # Source files for implemented component
+    lappend MOD "$ENTITY_BASE/network_mod_core_ftile.vhd"
 
 # $ARCHGRP == "DK-DEV-1SDX-P"
 } else {
@@ -84,14 +76,12 @@ if { $ARCHGRP == "400G1" } {
         [ list "RX_ETILE_ADAPTER"    "$RX_ADAPTER_BASE/eth_avst"     "FULL" ] \
     ]]
 
-    set MOD "$MOD $DK_1SDX_IP_BASE/etile_eth_1x100g.ip"
-    set MOD "$MOD $DK_1SDX_IP_BASE/etile_eth_4x25g.ip"
-    set MOD "$MOD $DK_1SDX_IP_BASE/etile_eth_4x10g.ip"
+    # IP are now in card top-level Modules.tcl
+    # Uncomment for network module synthesis only!
+    #lappend MOD "$DK_1SDX_IP_BASE/etile_eth_1x100g.ip"
+    #lappend MOD "$DK_1SDX_IP_BASE/etile_eth_4x25g.ip"
+    #lappend MOD "$DK_1SDX_IP_BASE/etile_eth_4x10g.ip"
 
-    set MOD "$MOD $ENTITY_BASE/network_mod_core_etile.vhd"
-
+    # Source files for implemented component
+    lappend MOD "$ENTITY_BASE/network_mod_core_etile.vhd"
 }
-
-set COMPONENTS [concat $COMPONENTS [list \
-        [ list "MI_INDIRECT_ACCESS"    "$MI_TOOLS_BASE/indirect_access"  "FULL" ] \
-    ]]
