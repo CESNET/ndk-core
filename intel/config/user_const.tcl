9a0e76ea17830585446571728eba7cda98eb3bb4
Jakub Cabal <cabal@cesnet.cz>
2022-05-03 14:46:46 +0200

    CORE: [FEATURE] Add PCIE_GEN parameter

--- intel/config/user_const.tcl
+++ intel/config/user_const.tcl
@@ -29,17 +29,23 @@ set ETH_PORT_SPEED(0)  400
 # NOTE: at this moment, all ports must have same number of channels!
 set ETH_PORT_CHAN(0)   1
 
-# PCIe parameters:
-# ================
-# PCIe endpoints (allowed values: 1, 2, 4):
+# PCIe parameters (not all combinations work):
+# ==============================================================================
+# PCIe Generation (possible values: 3, 4, 5):
+# 3 = PCIe Gen3
+# 4 = PCIe Gen4 (Stratix 10 with P-Tile or Agilex)
+# 5 = PCIe Gen5 (Agilex with R-Tile)
+set PCIE_GEN           4
+# PCIe endpoints (possible values: 1, 2, 4):
 # 1 = 1x PCIe x16 in one slot
 # 2 = 2x PCIe x16 in two slot OR 2x PCIe x8 in one slot (bifurcation x8+x8)
 # 4 = 4x PCIe x8 in two slot (bifurcation x8+x8)
 set PCIE_ENDPOINTS     1
-# PCIe endpoint mode (allowed values: 0, 1):
+# PCIe endpoint mode (possible values: 0, 1):
 # 0 = 1x16 lanes
 # 1 = 2x8 lanes (bifurcation x8+x8)
 set PCIE_ENDPOINT_MODE 1
+# ------------------------------------------------------------------------------
 
 # DMA parameters:
 # ===============
