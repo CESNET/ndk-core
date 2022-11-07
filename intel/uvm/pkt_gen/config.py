c5b1f3d866d0b10bffc2715c52249feecd0b6d84
Daniel Kriz <xkrizd01@vutbr.cz>
2022-11-07 12:42:12 +0100

    PKT GEN [MAINTENANCE]: Add weights to packet generator.

--- intel/uvm/pkt_gen/config.py
+++ intel/uvm/pkt_gen/config.py
@@ -13,4 +13,16 @@ class packet_config:
     def __init__(self):
         self.vlan = 4
         self.mpls = 4
+        #                      [IPv4(), IPv6(), MPLS(), Empty()]
+        self.mpls_weight     = [     1,      1,      1,      1]
+        #                      [IPv4(), IPv6(), MPLS(), Empty()]
+        self.ppp_weight      = [     1,      1,      1,      1]
+        #                      [IPv4(), IPv6(), VLAN(), MPLS(), Empty(), PPP()]
+        self.vlan_weight     = [     1,      1,      1,      1,       1,     1]
+        #                      [Payload(), Empty(), ICMPv4(), UDP(), TCP()]
+        self.l3_weight       = [        1,       1,        1,     1,     1]
+        #                      [Payload(), Empty()]
+        self.l4_weight       = [        1,       1]
+        #                      [IPv4(), IPv6(), VLAN(), MPLS(), Empty(), PPP()]
+        self.ethernet_weight = [     1,      1,      1,      1,       1,     1]
 
