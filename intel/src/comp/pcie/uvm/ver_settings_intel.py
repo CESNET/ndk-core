d339af5c4f838bb0e874db5e24826785c9b36b5d
Daniel Kriz <xkrizd01@vutbr.cz>
2023-06-15 15:40:15 +0200

    UVM PCIE [MAINTENANCE]: Add ver_settings.

---
+++ intel/src/comp/pcie/uvm/ver_settings_intel.py
@@ -0,0 +1,39 @@
+# ver_settings.py
+# Copyright (C) 2023 CESNET z. s. p. o.
+# Author(s): Daniel Kříž <danielkriz@cesnet.cz>
+
+SETTINGS = {
+    "default" : { # The default setting of verification is 512b and STRATIX with P_TILE
+        "CQ_MFB_REGIONS"       : "2"                 ,
+        "CQ_MFB_REGION_SIZE"   : "1"                 ,
+        "CQ_MFB_BLOCK_SIZE"    : "8"                 ,
+        "CQ_MFB_ITEM_WIDTH"    : "32"                ,
+
+        "RC_MFB_REGIONS"       : "2"                 ,
+        "RC_MFB_REGION_SIZE"   : "1"                 ,
+        "RC_MFB_BLOCK_SIZE"    : "8"                 ,
+        "RC_MFB_ITEM_WIDTH"    : "32"                ,
+
+        "CC_MFB_REGIONS"       : "2"                 ,
+        "CC_MFB_REGION_SIZE"   : "1"                 ,
+        "CC_MFB_BLOCK_SIZE"    : "8"                 ,
+        "CC_MFB_ITEM_WIDTH"    : "32"                ,
+
+        "RQ_MFB_REGIONS"       : "2"                 ,
+        "RQ_MFB_REGION_SIZE"   : "1"                 ,
+        "RQ_MFB_BLOCK_SIZE"    : "8"                 ,
+        "RQ_MFB_ITEM_WIDTH"    : "32"                ,
+
+        "AXI_CQUSER_WIDTH"     : "183"               ,
+        "AXI_CCUSER_WIDTH"     : "81"                ,
+        "AXI_RQUSER_WIDTH"     : "137"               ,
+        "AXI_RCUSER_WIDTH"     : "161"               ,
+        "AXI_STRADDLING"       : "0"                 ,
+
+        "DEVICE"               : "\\\"STRATIX10\\\"" ,
+        "PCIE_ENDPOINT_TYPE"   : "\\\"P_TILE\\\""    ,
+    },
+    "_combinations_" : (  
+    (), # Works the same as '("default",),' as the "default" is applied in every combination
+    ),
+}
