4cf00bb51022d85863c8bcdb6b9a4b291b27bd53
Daniel Kriz <xkrizd01@vutbr.cz>
2023-07-12 17:20:45 +0200

    UVM PCIE [MAINTENANCE]: Add support for new version of comparators.

--- intel/src/comp/pcie/uvm/tbench/env/scoreboard_cmp.sv
+++ intel/src/comp/pcie/uvm/tbench/env/scoreboard_cmp.sv
@@ -11,21 +11,21 @@ class scoreboard_mfb #(MFB_ITEM_WIDTH, MFB_BLOCK_SIZE) extends uvm_common::compa
         super.new(name, parent);
     endfunction
 
-    virtual function int unsigned compare(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function int unsigned compare(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         int unsigned model_len   = 0;
         int unsigned model_align = 0;
         int unsigned dut_len     = 0;
         int unsigned ret         = 1;
 
-        model_len = tr_model.data_tr.size();
-        dut_len   = tr_dut.size();
+        model_len = tr_model.item.data_tr.size();
+        dut_len   = tr_dut.in_item.size();
         model_align = ((model_len % MFB_BLOCK_SIZE) != 0) ? (MFB_BLOCK_SIZE - model_len % MFB_BLOCK_SIZE) : 0;
         `uvm_info(get_type_name(), this.message(tr_model, tr_dut), UVM_MEDIUM)
 
-        if (tr_model.error == '0) begin
+        if (tr_model.item.error == '0) begin
             if (dut_len >= model_len && dut_len <= (model_len + model_align)) begin
                 for (int unsigned it = 0; it < model_len; it++) begin
-                    if (!$isunknown(tr_model.data_tr.data[it]) && tr_model.data_tr.data[it] !== tr_dut.data[it]) begin
+                    if (!$isunknown(tr_model.item.data_tr.data[it]) && tr_model.item.data_tr.data[it] !== tr_dut.in_item.data[it]) begin
                         ret = 0;
                     end
                 end
@@ -34,7 +34,7 @@ class scoreboard_mfb #(MFB_ITEM_WIDTH, MFB_BLOCK_SIZE) extends uvm_common::compa
         return ret;
     endfunction
 
-    virtual function string message(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function string message(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         string msg = "";
         $swrite(msg, "%s\n\tDUT PACKET %s\n\n",   msg, tr_dut.convert2string());
         $swrite(msg, "%s\n\tMODEL PACKET%s\n\n",  msg, tr_model.convert2string());
@@ -52,27 +52,27 @@ class scoreboard_mfb_xilinx #(MFB_ITEM_WIDTH, MFB_BLOCK_SIZE, PCIE_TAG_WIDTH) ex
         super.new(name, parent);
     endfunction
 
-    virtual function int unsigned compare(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function int unsigned compare(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         int unsigned ret = 0;
 
-        if (tr_model.data_tr.data[2][15-1 : 11]) begin
-            ret = tr_model.data_tr.compare(tr_dut);
+        if (tr_model.item.data_tr.data[2][15-1 : 11]) begin
+            ret = tr_model.item.data_tr.compare(tr_dut.in_item);
         end else begin
-            if (tr_model.data_tr.data[0] !== tr_dut.data[0] || tr_model.data_tr.data[1] !== tr_dut.data[1] || tr_model.data_tr.data[2] !== tr_dut.data[2] || tr_model.data_tr.data[3][32-1 : 8] !== tr_dut.data[3][32-1 : 8]) begin
-                $write("tr_model.data_tr.data[0] %h tr_dut.data[0] %h\n", tr_model.data_tr.data[0], tr_dut.data[0]);
-                $write("tr_model.data_tr.data[1] %h tr_dut.data[1] %h\n", tr_model.data_tr.data[1], tr_dut.data[1]);
-                $write("tr_model.data_tr.data[2] %h tr_dut.data[2] %h\n", tr_model.data_tr.data[2], tr_dut.data[2]);
-                $write("tr_model.data_tr.data[3][32-1 : 8] %h tr_dut.data[3][32-1 : 8] %h\n", tr_model.data_tr.data[3][32-1 : 8], tr_dut.data[3][32-1 : 8]);
+            if (tr_model.item.data_tr.data[0] !== tr_dut.in_item.data[0] || tr_model.item.data_tr.data[1] !== tr_dut.in_item.data[1] || tr_model.item.data_tr.data[2] !== tr_dut.in_item.data[2] || tr_model.item.data_tr.data[3][32-1 : 8] !== tr_dut.in_item.data[3][32-1 : 8]) begin
+                $write("tr_model.item.data_tr.data[0] %h tr_dut.in_item.data[0] %h\n", tr_model.item.data_tr.data[0], tr_dut.in_item.data[0]);
+                $write("tr_model.item.data_tr.data[1] %h tr_dut.in_item.data[1] %h\n", tr_model.item.data_tr.data[1], tr_dut.in_item.data[1]);
+                $write("tr_model.item.data_tr.data[2] %h tr_dut.in_item.data[2] %h\n", tr_model.item.data_tr.data[2], tr_dut.in_item.data[2]);
+                $write("tr_model.item.data_tr.data[3][32-1 : 8] %h tr_dut.in_item.data[3][32-1 : 8] %h\n", tr_model.item.data_tr.data[3][32-1 : 8], tr_dut.in_item.data[3][32-1 : 8]);
                 ret = 0;
             end else begin
                 ret = 1;
             end
-            tag_man.code(tr_dut.data[3][8-1 : 0], tr_model.data_tr.data[3][8-1 : 0]);
+            tag_man.code(tr_dut.in_item.data[3][8-1 : 0], tr_model.item.data_tr.data[3][8-1 : 0]);
         end
         return ret;
     endfunction
 
-    virtual function string message(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function string message(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         string msg = "";
         $swrite(msg, "%s\n\tDUT PACKET %s\n\n",   msg, tr_dut.convert2string());
         $swrite(msg, "%s\n\tMODEL PACKET%s\n\n",  msg, tr_model.convert2string());
@@ -90,21 +90,21 @@ class scoreboard_mvb #(AVST_UP_META_W, PCIE_TAG_WIDTH, type CLASS_TYPE) extends 
         super.new(name, parent);
     endfunction
 
-    virtual function int unsigned compare(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function int unsigned compare(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         int unsigned ret = 1;
-        if (tr_model.data[126]) begin
-            ret = tr_model.compare(tr_dut);
+        if (tr_model.item.data[126]) begin
+            ret = tr_model.item.compare(tr_dut.in_item);
         end else begin
             // TODO: in model add support for tag translation
-            if (tr_model.data[72-1 : 0] !== tr_dut.data[72-1 : 0] || tr_model.data[AVST_UP_META_W-1 : 80] !== tr_dut.data[AVST_UP_META_W-1 : 80]) begin
+            if (tr_model.item.data[72-1 : 0] !== tr_dut.in_item.data[72-1 : 0] || tr_model.item.data[AVST_UP_META_W-1 : 80] !== tr_dut.in_item.data[AVST_UP_META_W-1 : 80]) begin
                 ret = 0;
             end
-            tag_man.code(tr_dut.data[80-1 : 72], tr_model.data[80-1 : 72]);
+            tag_man.code(tr_dut.in_item.data[80-1 : 72], tr_model.item.data[80-1 : 72]);
         end
         return ret;
     endfunction
 
-    virtual function string message(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function string message(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         string msg = "";
         $swrite(msg, "%s\n\tDUT PACKET %s\n\n",   msg, tr_dut.convert2string());
         $swrite(msg, "%s\n\tMODEL PACKET%s\n\n",  msg, tr_model.convert2string());
@@ -120,15 +120,15 @@ class scoreboard_mtc_mfb_xilinx #(MFB_ITEM_WIDTH) extends uvm_common::comparer_b
         super.new(name, parent);
     endfunction
 
-    virtual function int unsigned compare(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function int unsigned compare(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         int unsigned ret         = 1;
 
-        ret = tr_model.data_tr.compare(tr_dut);
+        ret = tr_model.item.data_tr.compare(tr_dut.in_item);
 
         return ret;
     endfunction
 
-    virtual function string message(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
+    virtual function string message(uvm_common::model_item #(MODEL_ITEM) tr_model, uvm_common::dut_item #(DUT_ITEM) tr_dut);
         string msg = "";
         $swrite(msg, "%s\n\tDUT PACKET %s\n\n",   msg, tr_dut.convert2string());
         $swrite(msg, "%s\n\tMODEL PACKET%s\n\n",  msg, tr_model.convert2string());
