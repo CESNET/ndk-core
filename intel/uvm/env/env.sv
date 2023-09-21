98607acc8ff3d0502157c3028f8ed3a16c2681fb
Jakub Cabal <cabal@cesnet.cz>
2023-09-21 09:01:19 +0200

    CORE: [FEATURE] Add model_timeout parameter to env function in common APP UVM scoreboard

--- intel/uvm/env/env.sv
+++ intel/uvm/env/env.sv
@@ -182,8 +182,8 @@ class env #(ETH_STREAMS, ETH_PKT_MTU, ETH_RX_HDR_WIDTH, ETH_TX_HDR_WIDTH, DMA_ST
         return m_scoreboard.model_get();
     endfunction
 
-    function void delay_max_set(time delay_max);
-        m_scoreboard.delay_max_set(delay_max);
+    function void delay_max_set(time delay_max, time model_timeout = 1ms);
+        m_scoreboard.delay_max_set(delay_max, model_timeout);
     endfunction
 
     function void connect_phase(uvm_phase phase);
