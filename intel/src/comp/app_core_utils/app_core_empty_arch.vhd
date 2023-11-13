3769d7ccc7cd63bc3148fc8c472ed581e98e3da1
Vladislav Valek <vladislav.valek@email.cz>
2023-11-13 11:02:21 +0100

    app_core_empty_arch [BUGFIX]: remove loopbacks when ETH/DMA disabled
    This causes the combinatorial loops when there is a second loopback of
    this sort.

--- intel/src/comp/app_core_utils/app_core_empty_arch.vhd
+++ intel/src/comp/app_core_utils/app_core_empty_arch.vhd
@@ -23,60 +23,32 @@ begin
     APP_RESET    <= RESET_USER_X2;
 
     ETH_RX_MVB_DST_RDY <= (others => '1');
+    ETH_RX_MFB_DST_RDY <= (others => '1');
 
-    eth_loopback_g : if (ETH_STREAMS > 0) generate
-        ETH_TX_MFB_DATA    <= ETH_RX_MFB_DATA;
-        ETH_TX_MFB_HDR     <= (others => '0');
-        ETH_TX_MFB_SOF     <= ETH_RX_MFB_SOF;
-        ETH_TX_MFB_EOF     <= ETH_RX_MFB_EOF;
-        ETH_TX_MFB_SOF_POS <= ETH_RX_MFB_SOF_POS;
-        ETH_TX_MFB_EOF_POS <= ETH_RX_MFB_EOF_POS;
-        ETH_TX_MFB_SRC_RDY <= ETH_RX_MFB_SRC_RDY;
-        ETH_RX_MFB_DST_RDY <= ETH_TX_MFB_DST_RDY;
-    else generate
-        ETH_TX_MFB_DATA    <= (others => '0');
-        ETH_TX_MFB_HDR     <= (others => '0');
-        ETH_TX_MFB_SOF     <= (others => '0');
-        ETH_TX_MFB_EOF     <= (others => '0');
-        ETH_TX_MFB_SOF_POS <= (others => '0');
-        ETH_TX_MFB_EOF_POS <= (others => '0');
-        ETH_TX_MFB_SRC_RDY <= (others => '0');
-        ETH_RX_MFB_DST_RDY <= (others => '1');
-    end generate;
+    ETH_TX_MFB_DATA    <= (others => '0');
+    ETH_TX_MFB_HDR     <= (others => '0');
+    ETH_TX_MFB_SOF     <= (others => '0');
+    ETH_TX_MFB_EOF     <= (others => '0');
+    ETH_TX_MFB_SOF_POS <= (others => '0');
+    ETH_TX_MFB_EOF_POS <= (others => '0');
+    ETH_TX_MFB_SRC_RDY <= (others => '0');
 
-    dma_loopback_g : if (DMA_TYPE /= 0) generate
-        DMA_RX_MVB_LEN      <= DMA_TX_MVB_LEN;
-        DMA_RX_MVB_HDR_META <= DMA_TX_MVB_HDR_META;
-        DMA_RX_MVB_CHANNEL  <= DMA_TX_MVB_CHANNEL;
-        DMA_RX_MVB_DISCARD  <= (others => '0');
-        DMA_RX_MVB_VLD      <= DMA_TX_MVB_VLD;
-        DMA_RX_MVB_SRC_RDY  <= DMA_TX_MVB_SRC_RDY;
-        DMA_TX_MVB_DST_RDY  <= DMA_RX_MVB_DST_RDY;
+    DMA_RX_MVB_LEN      <= (others => '0');
+    DMA_RX_MVB_HDR_META <= (others => '0');
+    DMA_RX_MVB_CHANNEL  <= (others => '0');
+    DMA_RX_MVB_DISCARD  <= (others => '0');
+    DMA_RX_MVB_VLD      <= (others => '0');
+    DMA_RX_MVB_SRC_RDY  <= (others => '0');
 
-        DMA_RX_MFB_DATA    <= DMA_TX_MFB_DATA;
-        DMA_RX_MFB_SOF     <= DMA_TX_MFB_SOF;
-        DMA_RX_MFB_EOF     <= DMA_TX_MFB_EOF;
-        DMA_RX_MFB_SOF_POS <= DMA_TX_MFB_SOF_POS;
-        DMA_RX_MFB_EOF_POS <= DMA_TX_MFB_EOF_POS;
-        DMA_RX_MFB_SRC_RDY <= DMA_TX_MFB_SRC_RDY;
-        DMA_TX_MFB_DST_RDY <= DMA_RX_MFB_DST_RDY;
-    else generate
-        DMA_RX_MVB_LEN      <= (others => '0');
-        DMA_RX_MVB_HDR_META <= (others => '0');
-        DMA_RX_MVB_CHANNEL  <= (others => '0');
-        DMA_RX_MVB_DISCARD  <= (others => '0');
-        DMA_RX_MVB_VLD      <= (others => '0');
-        DMA_RX_MVB_SRC_RDY  <= (others => '0');
-        DMA_TX_MVB_DST_RDY  <= (others => '1');
+    DMA_RX_MFB_DATA    <= (others => '0');
+    DMA_RX_MFB_SOF     <= (others => '0');
+    DMA_RX_MFB_EOF     <= (others => '0');
+    DMA_RX_MFB_SOF_POS <= (others => '0');
+    DMA_RX_MFB_EOF_POS <= (others => '0');
+    DMA_RX_MFB_SRC_RDY <= (others => '0');
 
-        DMA_RX_MFB_DATA    <= (others => '0');
-        DMA_RX_MFB_SOF     <= (others => '0');
-        DMA_RX_MFB_EOF     <= (others => '0');
-        DMA_RX_MFB_SOF_POS <= (others => '0');
-        DMA_RX_MFB_EOF_POS <= (others => '0');
-        DMA_RX_MFB_SRC_RDY <= (others => '0');
-        DMA_TX_MFB_DST_RDY <= (others => '1');
-    end generate;
+    DMA_TX_MVB_DST_RDY <= (others => '1');
+    DMA_TX_MFB_DST_RDY <= (others => '1');
 
     MEM_AVMM_READ       <= (others => '0');
     MEM_AVMM_WRITE      <= (others => '0');
