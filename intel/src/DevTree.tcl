6f779e592a760f07ff3685a61853920944d37b7a
Daniel Kondys <xkondy00@vutbr.cz>
2024-02-15 13:19:40 +0100

    FPGA_COMMON: [FEATURE] Add PCIe Core to the Device Tree

--- intel/src/DevTree.tcl
+++ intel/src/DevTree.tcl
@@ -13,7 +13,7 @@ proc dts_build_netcope {} {
     set ADDR_ETH_MAC    "0x00008000"
     set ADDR_JTAG_IP    "0x00010000"
     set ADDR_DMA_MOD    "0x01000000"
-    #set ADDR_MSIX_MOD   "0x01400000"
+    set ADDR_PCIE_DBG   "0x01400000"
     set ADDR_ETH_PCS    "0x00800000"
     set ADDR_USERAPP    "0x02000000"
 
@@ -135,6 +135,10 @@ proc dts_build_netcope {} {
         set    gls_offset [expr $i * 0x200]
         append ret [dts_gen_loop_switch [expr $ADDR_GEN_LOOP + $gls_offset] "dbg_gls$i"]
     }
+
+    # PCIe Debug
+    global PCIE_ENDPOINT_MODE PCIE_MOD_ARCH
+    append ret [dts_pcie_core_dbg $ADDR_PCIE_DBG $PCIE_ENDPOINTS $PCIE_ENDPOINT_MODE $PCIE_MOD_ARCH]
 
     append ret "};"
 
