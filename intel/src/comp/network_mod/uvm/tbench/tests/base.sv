be6ba485050fa96dfda0e567b65f2f72aac70945
Radek Isa <isa@cesnet.cz>
2024-02-27 17:50:53 +0100

    NETWORK MOD UVM: [MAINTENANCE] Remove unusefull classes parameters

--- intel/src/comp/network_mod/uvm/tbench/tests/base.sv
+++ intel/src/comp/network_mod/uvm/tbench/tests/base.sv
@@ -6,19 +6,8 @@
 
 
 class base#(
-    string       ETH_CORE_ARCH    ,
     int unsigned ETH_PORTS        ,
-    int unsigned ETH_PORT_SPEED[ETH_PORTS-1:0],
-
     int unsigned ETH_PORT_CHAN[ETH_PORTS-1:0]    ,
-    int unsigned EHIP_PORT_TYPE[ETH_PORTS-1:0]   ,
-    int unsigned ETH_PORT_RX_MTU[ETH_PORTS-1:0]  ,
-    int unsigned ETH_PORT_TX_MTU[ETH_PORTS-1:0]  ,
-    int unsigned LANES            ,
-    int unsigned QSFP_PORTS       ,
-    int unsigned QSFP_I2C_PORTS   ,
-    int unsigned QSFP_I2C_TRISTATE,
-
     int unsigned ETH_TX_HDR_WIDTH,
     int unsigned ETH_RX_HDR_WIDTH,
 
@@ -28,24 +17,13 @@ class base#(
     int unsigned ITEM_WIDTH       ,
 
     int unsigned MI_DATA_WIDTH    ,
-    int unsigned MI_ADDR_WIDTH    ,
+    int unsigned MI_ADDR_WIDTH)
+extends uvm_test;
+    typedef uvm_component_registry#(test::base#(ETH_PORTS, ETH_PORT_CHAN, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH, MI_ADDR_WIDTH
+                            ), "test::base") type_id;
 
-    int unsigned MI_DATA_WIDTH_PHY,
-    int unsigned MI_ADDR_WIDTH_PHY,
-
-    int unsigned LANE_RX_POLARITY ,
-    int unsigned LANE_TX_POLARITY ,
-    int unsigned RESET_WIDTH      ,
-    string DEVICE           ,
-    string BOARD
-) extends uvm_test;
-    typedef uvm_component_registry#(test::base#(ETH_CORE_ARCH, ETH_PORTS, ETH_PORT_SPEED, ETH_PORT_CHAN, EHIP_PORT_TYPE, ETH_PORT_RX_MTU, ETH_PORT_TX_MTU, LANES, QSFP_PORTS,
-                            QSFP_I2C_PORTS, QSFP_I2C_TRISTATE, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH,
-                            MI_ADDR_WIDTH, MI_DATA_WIDTH_PHY, MI_ADDR_WIDTH_PHY, LANE_RX_POLARITY, LANE_TX_POLARITY, RESET_WIDTH, DEVICE, BOARD), "test::base") type_id;
-
-    uvm_network_mod_env::env#(ETH_CORE_ARCH, ETH_PORTS, ETH_PORT_SPEED, ETH_PORT_CHAN, EHIP_PORT_TYPE, ETH_PORT_RX_MTU, ETH_PORT_TX_MTU, LANES, QSFP_PORTS,
-                            QSFP_I2C_PORTS, QSFP_I2C_TRISTATE, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH,
-                            MI_ADDR_WIDTH, MI_DATA_WIDTH_PHY, MI_ADDR_WIDTH_PHY, LANE_RX_POLARITY, LANE_TX_POLARITY, RESET_WIDTH, DEVICE, BOARD) m_env;
+    uvm_network_mod_env::env#(ETH_PORTS, ETH_PORT_CHAN, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH,
+                            MI_ADDR_WIDTH) m_env;
     localparam time timeout_max = 200us;
 
     // ------------------------------------------------------------------------
@@ -63,9 +41,8 @@ class base#(
     endfunction
 
     function void build_phase(uvm_phase phase);
-        m_env = uvm_network_mod_env::env#(ETH_CORE_ARCH, ETH_PORTS, ETH_PORT_SPEED, ETH_PORT_CHAN, EHIP_PORT_TYPE, ETH_PORT_RX_MTU, ETH_PORT_TX_MTU, LANES, QSFP_PORTS,
-                            QSFP_I2C_PORTS, QSFP_I2C_TRISTATE, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH,
-                            MI_ADDR_WIDTH, MI_DATA_WIDTH_PHY, MI_ADDR_WIDTH_PHY, LANE_RX_POLARITY, LANE_TX_POLARITY, RESET_WIDTH, DEVICE, BOARD)::type_id::create("m_env", this);
+        m_env = uvm_network_mod_env::env#(ETH_PORTS, ETH_PORT_CHAN, ETH_TX_HDR_WIDTH, ETH_RX_HDR_WIDTH, REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, MI_DATA_WIDTH,
+                            MI_ADDR_WIDTH)::type_id::create("m_env", this);
     endfunction
 
     // ------------------------------------------------------------------------
